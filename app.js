9a0c70b01fe6620dfe435730ceaeca01a2eb3499
Maxime <maxime.attala@edu.devinci.fr>
2022-01-25 19:03:11 +0100

    add: api

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const path         = require('path');
 const cookieParser = require('cookie-parser');
 const logger       = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login');
-const subscriptionRouter = require('./routes/subscription');
-const chatRouter = require('./routes/chat');
+const viewsRouter = require('./routes/views');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -20,11 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/subscription', subscriptionRouter);
-app.use('/chat', chatRouter);
+app.use('/', viewsRouter);
+app.use('/api', apiRouter);
 
 
 // catch 404 and forward to error handler
