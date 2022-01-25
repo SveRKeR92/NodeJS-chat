b2e0effd1e92261958f45ffbc9bf10b0e8ab3b16
Maxime <maxime.attala@edu.devinci.fr>
2022-01-25 16:10:11 +0100

    status optionnal

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).json({msg: 'default route'})
+  res.json({msg: 'default route'})
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.status.json({msg: 'hello world'})
+  res.status(200).json({msg: 'hello world'})
 });
 
 module.exports = router;
