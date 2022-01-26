b6123dcadedc9e2592da5cbbd890f4895119f8e4
Maxime <maxime.attala@edu.devinci.fr>
2022-01-26 15:10:47 +0100

    api:get all users for db at /api/users

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const createError  = require('http-errors');
-const express      = require('express');
-const path         = require('path');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger       = require('morgan');
+const logger = require('morgan');
 
 const viewsRouter = require('./routes/views');
 const apiRouter = require('./routes/api');
 
 const app = express();
-
 // view engine setup
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,11 +44,12 @@ app.use(
     function (err, req, res, next) {
         // set locals, only providing error in development
         res.locals.message = err.message;
-        res.locals.error   = req.app.get('env') === 'development' ? err : {};
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
 
         // render the error page
         res.status(err.status || 500);
         res.render('error');
-    });
+    }
+);
 
 module.exports = app;
