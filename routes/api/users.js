90d594cda3cad594f3b0ec5afeb84ee3aede8c2f
Maxime <maxime.attala@edu.devinci.fr>
2022-01-26 16:37:50 +0100

    fix api urls

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,13 +13,12 @@ router.get('/',
      * @param {express.NextFunction} next 
      */
     async (req, res, next) => {
-
-
         const allUsers = await prisma.users.findMany()
         res.json(allUsers);
-    });
+    }
+);
 
-router.get('/get:userId',
+router.get('/:userId',
     /**
      * @param {express.Request<{userId: string}>} req 
      * @param {express.Response} res 
@@ -29,17 +28,18 @@ router.get('/get:userId',
         const userId = Number.parseInt(req.params.userId);
         const theUser = await prisma.users.findUnique({where : {id: userId}})
         res.json(theUser).status(200);
-    });
+    }
+);
 
-router.post('/get:userId',
-    /**
-     * @param {express.Request<{userId: string}>} req 
-     * @param {express.Response} res 
-     * @param {express.NextFunction} next 
-     */
-    async (req, res, next) => {
-        res.json(req.params.userId).status(200);
-    });
+// router.post('/get:userId',
+//     /**
+//      * @param {express.Request<{userId: string}>} req 
+//      * @param {express.Response} res 
+//      * @param {express.NextFunction} next 
+//      */
+//     async (req, res, next) => {
+//         res.json(req.params.userId).status(200);
+//     });
 
 router.get('/create',
     /**
@@ -57,6 +57,9 @@ router.get('/create',
             }
         })
         res.json(createUser).status(200);
-    });
+    }
+);
+
+
 
 module.exports = router;
