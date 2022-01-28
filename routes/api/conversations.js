078269cadb83bf681393b06ceb9d39acc3b24bd9
Alexandre <alexandre.vauthier@edu.devinci.fr>
2022-01-28 15:13:31 +0100

    api conversation get router added

--- routes/api/conversations.js
+++ routes/api/conversations.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-router.get("/conversation/:conv_id", 
+router.get("/conversation/:convId", 
     /**
     * @param {express.Request} req 
     * @param {express.Response} res 
     * @param {express.NextFunction} next 
     */
     async (req, res, next) => {
-        const userId = Number.parseInt(req.params.userId);
-        const theUser = await prisma.users.findUnique({where : {id: userId}})
-        res.json(theUser).status(200);
+        const convId = Number.parseInt(req.params.convId);
+        const theConversation = await prisma.conversations.findUnique({where : {id: convId}})
+        res.json(theConversation).status(200);
     }
 )
